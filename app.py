b9aafccd08f94bb1ebd2f3daa6b175081ab88b46
Enes Alkan <enesalkan.rbdd@gmail.com>
2025-06-07 21:35:38 +0300

    Log: Print directory contents to debug model_catdv03.pkl

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import joblib
 import numpy as np
+import os
+print("Render içeriği:", os.listdir())
 
 # Doğru modeli yükle
 model = joblib.load("model_catdv01.pkl")
@@ -29,14 +31,34 @@ class Inputs(BaseModel):
     x8: float  # IV09
 
 @app.post("/predict_catdv01")
+
 def predict_catdv01(inp: Inputs):
-    # Log dönüşüm gerekenler
     log_x4 = np.log(inp.x4)
     log_x5 = np.log(inp.x5)
     log_x6 = np.log(inp.x6)
 
-    # CATDV01 modeline uygun veri düzenlemesi
     X = np.array([[inp.x1, inp.x2, inp.x3, log_x4, log_x5, log_x6, inp.x8]])
     y_hat = model.predict(X)[0]
+
+    print("RandomForest predicted:", y_hat)
+
     return {"result": "A" if y_hat else "B"}
+
+# Load CATDV03 model
+model_catdv03 = joblib.load("model_catdv03.pkl")
+
+@app.post("/predict_catdv03")
+def predict_catdv03(inp: Inputs):
+    log_x4 = np.log(inp.x4)
+    log_x5 = np.log(inp.x5)
+
+    # CATDV03 modeline uygun input dizisi
+    X = np.array([[inp.x1, inp.x2, inp.x3, log_x4, log_x5, inp.x7, inp.x8]])
+
+    y_hat = model_catdv03.predict(X)[0]
+
+    print("CATDV03 predicted:", y_hat)
+
+    return {"result": "A" if y_hat else "B"}
+
 
